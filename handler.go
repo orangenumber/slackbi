88c2d452e4b1c58bb72086465c608295ad3fa470
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-22 17:04:37 -0600

    Update handler.go

--- handler.go
+++ handler.go
@@ -4,98 +4,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/orangenumber/areq"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"os/exec"
 )
-
-type msgReq struct {
-	Event struct {
-		BotID        string `json:"bot_id"`
-		Type         string `json:"type"`
-		Subtype      string `json:"subtype"`
-		User         string `json:"user"`
-		Text         string `json:"text"`
-		TS           string `json:"ts"`
-		Channel      string `json:"channel"`
-		ParentUserID string `json:"parent_user_id"`
-		EventTS      string `json:"event_ts"`
-		ChannelType  string `json:"channel_type"`
-	} `json:"event"`
-}
-
-// Text() will return without mention
-func (r *msgReq) Text() string {
-	return RemoveMention(r.Event.Text)
-}
-
-func (r *msgReq) TextRaw() string {
-	return r.Event.Text
-}
-
-func (r *msgReq) RespFile(slackToken string, filename string) (err error) {
-	//curl -F file=@test.txt \
-	//	-F "initial_comment=Hello, Leadville" \
-	//	-F channels=D01HSN5P7JM \
-	//	-H "Authorization: Bearer xoxb-132329662322-1592329468787-GwGCuTncBxv6ioP8nLpOiLku" \
-	//	https://slack.com/api/files.upload
-
-	cmd := exec.Command("curl",
-		`-F file=@`+filename,
-		`-F "initial_comment=result"`,
-		`-F "channels=`+r.Event.Channel+`"`,
-		`-H "Authorization: Bearer`+slackToken+`"`,
-		`https://slack.com/api/files.upload`)
-	log.Printf("Running command and waiting for it to finish...")
-
-	for _, v := range cmd.Args {
-		println(v)
-	}
-	return cmd.Run()
-}
-
-func (r *msgReq) RespText(slackToken string, replyToThread bool, txt string) error {
-	msg_resp := msgResp{
-		Token:   slackToken,
-		Channel: r.Event.Channel,
-		Text:    txt, // "you said " + RemoveMention(msg_req.Event.Text),
-		//ThreadTS: msg_req.Event.TS,
-	}
-	if replyToThread {
-		msg_resp.ThreadTS = r.Event.TS
-	}
-	return msg_resp.Send()
-}
-
-type msgResp struct {
-	Token    string `json:"token"`
-	Channel  string `json:"channel"`
-	Text     string `json:"text"`
-	ThreadTS string `json:"thread_ts"`
-}
-
-func (r *msgResp) Send() error {
-	reqData, err := json.Marshal(r)
-	if err != nil {
-		return err
-	}
-	_, err = areq.Request("POST", "https://slack.com/api/chat.postMessage",
-		areq.PluginFn{
-			Name: "Authorization Header",
-			FnPre: func(d *areq.AReq) {
-				d.Req.Header.Set("Authorization", "Bearer "+r.Token)
-			},
-		},
-		areq.Plugin.SetBody(bytes.NewReader(reqData), "json"),
-	)
-	// Response
-	if err != nil {
-		return err
-	}
-	return nil
-}
 
 type msgChallenge struct {
 	Type      string `json:"type,omitEmpty"`
@@ -103,11 +14,14 @@ type msgChallenge struct {
 	Challenge string `json:"challenge,omitEmpty"`
 }
 
-func (b *SlackBot) serve() error {
+func (b *SBI) serve() error {
 	http.HandleFunc(b.config.Service.Path, func(w http.ResponseWriter, r *http.Request) {
 		if body, err := ioutil.ReadAll(r.Body); err == nil {
+			// ====================================================================
 			// Handling challenge
+			// ====================================================================
 			if b.config.Service.AcceptChallege {
+				// {"token":"XLVmORnn6ha4XWXDqbcxfW0h","challenge":"1Gu5OMmDSab2rbxRKk6gIC3C5d8DmQ9Jza3VYtJgTdxHCItVzq7v","type":"url_verification"}
 				if bytes.ContainsAny(body, "challenge") {
 					var chall msgChallenge
 					if err := json.Unmarshal(body, &chall); err == nil {
@@ -118,37 +32,37 @@ func (b *SlackBot) serve() error {
 				}
 			}
 
-			// {"token":"XLVmORnn6ha4XWXDqbcxfW0h","challenge":"1Gu5OMmDSab2rbxRKk6gIC3C5d8DmQ9Jza3VYtJgTdxHCItVzq7v","type":"url_verification"}
-			var msg_req msgReq
-
-			//if json.Unmarshal(body, &msg_req) == nil && (msg_req.Event.User != "" && msg_req.Event.Subtype != "bot_message") {
-			if json.Unmarshal(body, &msg_req) == nil && (msg_req.Event.BotID == "") {
-				b.logger.Debugf("MSG received, From=%s, Msg=%s, Thread=%s", msg_req.Event.User, msg_req.Event.Text, msg_req.Event.TS)
+			// ====================================================================
+			// Regular message
+			// ====================================================================
+			var msg_received MsgIncoming
+			if err := json.Unmarshal(body, &msg_received); err != nil {
+				b.logger.Debugf("MSG received but can't unmarshal, data=%s, err=%s", string(body), err.Error())
+				b.logger.Errorf("MSG received but can't unmarshal, err=%s", err.Error())
+			} else if msg_received.Event.BotID == "" || (msg_received.Event.User != "" && msg_received.Event.SubType != "bot_message") {
+				// ====================================================================
+				// Consider a legit message here
+				// ====================================================================
+				b.logger.Debugf("MSG msg_received, From=%s, Msg=%s, Thread=%s", msg_received.Event.User, msg_received.Event.Text, msg_received.Event.TS)
 				b.logger.Tracef("MSG raw=%s", string(body))
 
-				//msg_req.RespFile(b.config.SlackToken, []byte("hello"), msg_req.Text(), "txt")
+				// ====================================================================
+				// `command` will handle parse and sending response back.
+				// Bot will kick off command, but will not wait for the result to come
+				// out. This is due to some process take unexpected time.
+				// ====================================================================
+				go b.command(msg_received)
 
-				//msg_resp := msgResp{
-				//	Token:   b.config.SlackToken,
-				//	Channel: msg_req.Event.Channel,
-				//	Text:    "you said " + RemoveMention(msg_req.Event.Text),
-				//	//ThreadTS: msg_req.Event.TS,
-				//}
-
-				if err := msg_req.RespFile(b.config.SlackToken, "test.go"); err != nil {
-					b.logger.Errorf("MSG respond failed, Thread=%s, Channel=%s, Msg=%s, Err=%s", msg_req.Event.TS, msg_req.Event.Channel, msg_req.Event.Text, err.Error())
-				} else {
-					b.logger.Debugf("MSG respond, Thread=%s, Channel=%s", msg_req.Event.TS, msg_req.Event.Channel)
-				}
-
-				w.Write([]byte("ok"))
 			} else {
-				b.logger.Tracef("MSG received but ignored, TS=%s", msg_req.Event.TS)
+				// Consider a reply of bot itself.... will ignore for now..
+				b.logger.Tracef("MSG msg_received but ignored, TS=%s", msg_received.Event.TS)
 			}
+			w.Write([]byte("ok"))
 		} else {
 			w.WriteHeader(200)
 			w.Write([]byte("Unexpected: " + err.Error()))
+			b.logger.Errorf("MSG received but failed to read, err=%s", err.Error())
 		}
 	})
-	return http.ListenAndServe(fmt.Sprintf("%s:%d", b.config.Service.Address, b.config.Service.Port), nil)
+	return http.ListenAndServe(fmt.Sprintf("%s%s", b.config.Service.Address, b.config.Service.Port), nil)
 }
