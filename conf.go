ed46b239629fb8d6b2de5342077004f024575b34
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-22 17:05:32 -0600

    Update and rename config.go to conf.go

---
+++ conf.go
@@ -0,0 +1,55 @@
+package slackbi
+
+import (
+	"encoding/json"
+	aface "github.com/gonyyi/aFace/logger"
+	"io/ioutil"
+)
+
+type config struct {
+	SlackToken string `json:"slack_token"`
+	BotVersion string `json:"bot_version"`
+	BotName    string `json:"bot_name"`
+	Logging    bool   `json:"logging"`
+	logger     aface.Logger1a
+	Service    struct {
+		Address        string `json:"address"`
+		Port           string `json:"port"`
+		Path           string `json:"path"`
+		AcceptChallege bool   `json:"accept_challenge"` // this is one time URL verification from slack. (https://api.slack.com/events/url_verification)
+	} `json:"service"`
+	Module struct {
+		Dir  string `json:"directory"`
+		Conf string `json:"config_filename"`
+	} `json:"module"`
+}
+
+func (c *config) Save(filename string) error {
+	j, err := json.MarshalIndent(c, "", "   ")
+	if err != nil {
+		return err
+	}
+	if err = ioutil.WriteFile(filename, j, 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewConfig() *config {
+	c := config{}
+	c.Default()
+	return &c
+}
+
+func ReadConfig(filename string) (*config, error) {
+	if b, err := ioutil.ReadFile(filename); err != nil {
+		return nil, err
+	} else {
+		c := config{}
+		if err := json.Unmarshal(b, &c); err != nil {
+			return nil, err
+		}
+		return &c, nil
+	}
+}
