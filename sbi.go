d9ff20576a3bc11ac41b012296dd282f56f2c109
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-22 17:03:41 -0600

    Update sbi.go

--- sbi.go
+++ sbi.go
@@ -1,55 +1,45 @@
 package slackbi
 
 import (
-	"github.com/gonyyi/alog"
+	"fmt"
+	aface "github.com/gonyyi/aFace/logger"
 	"github.com/gonyyi/aninterface"
-	"strings"
 )
 
-type SlackBot struct {
-	config config
+type SBI struct {
+	config *config
 	logger aninterface.Logger1a
 }
 
 const (
-	VERSION = "1.0.0 (2020-12-21)"
-
-	DEFAULT_BOT_NAME           = "TestBot"
-	DEFAULT_BOT_VERSION        = "0.0.1"
-	DEFAULT_SERVICE_PORT       = 8080
-	DEFAULT_SERVICE_PATH       = "/"
-	DEFAULT_SERVICE_MODULE_DIR = "./modules"
+	SBI_VERSION                = "1.0.0 (2020-12-21)"
+	SLACK_ENDPOINT_MSG         = "https://slack.com/api/chat.postMessage"
+	SLACK_ENDPOINT_FILE_UPLOAD = "https://slack.com/api/files.upload"
 )
 
-func New(c config) *SlackBot {
-	b := &SlackBot{
+func New(c *config, logger aface.Logger1a) (*SBI, error) {
+	if c == nil {
+		return nil, fmt.Errorf("config is empty")
+	}
+	b := &SBI{
 		config: c,
 	}
-	if c.Logging.Enable {
-		b.logger = alog.New(c.Logging.output, "sbi ", alog.FDefault|alog.FDateYYYYMMDD|alog.FPrefix|alog.FLevelTrace)
+
+	if c.Logging && logger != nil {
+		b.logger = logger
 	} else {
-		b.logger = &aninterface.DummyLogger1a{} // to prevent null ptr error
+		b.logger = &aface.DummyLogger1a{} // to prevent null ptr error
 	}
 
-	b.logger.Infof("SlackBotInterface %s", VERSION)
+	b.logger.Infof("SlackBotInterface %s", SBI_VERSION)
 	b.logger.Infof("Creating %s: %s", b.config.BotName, b.config.BotVersion)
 
-	// check service
-	if b.config.Service.Port == 0 {
-		b.logger.Warnf("Invalid service port (:%d) -> using default port (:%d) instead", b.config.Service.Port, DEFAULT_SERVICE_PORT)
-		b.config.Service.Port = DEFAULT_SERVICE_PORT
-	}
-	if !strings.HasPrefix(b.config.Service.Path, "/") {
-		newPath := "/" + b.config.Service.Path
-		b.logger.Warnf("Invalid service path (%s) -> using (%s) instead", b.config.Service.Path, newPath)
-		b.config.Service.Path = newPath
-	}
+	b.config.Validate()
 
-	return b
+	return b, nil
 }
 
-func (b *SlackBot) Run() error {
-	b.logger.Infof("Serving HTTP %s:%d%s", b.config.Service.Address, b.config.Service.Port, b.config.Service.Path)
-	b.serve()
-	return nil
+func (b *SBI) Run() error {
+	b.logger.Infof("Serving HTTP <%s%s%s>", b.config.Service.Address, b.config.Service.Port, b.config.Service.Path)
+	return b.serve()
 }
