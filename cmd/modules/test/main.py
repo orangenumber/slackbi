07594d5ace4f9f389c8bc425e6829ca3db2f2fa1
Gon <29622355+gonyyi@users.noreply.github.com>
2020-12-28 17:02:22 -0600

    remove cmd

--- cmd/modules/test/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-if len(sys.argv):  # this should be always 3: self, arg1(userid), arg2(rest)
-    print "stdIN: <%s>" % sys.stdin.readlines()
-
-    sys.stderr.write("py-stderr\n")
-    sys.stdout.write("py-stdout\n")
